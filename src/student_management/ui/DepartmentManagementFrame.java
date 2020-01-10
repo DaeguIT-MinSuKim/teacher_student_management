f420d0fe3b7e251009448d8c5e0103227a7ff5d1
teacher <net94.teacher@gmail.com>
2020-01-10 16:57:12 +0900

    ManagementFrame  추상화

--- src/student_management/ui/DepartmentManagementFrame.java
+++ src/student_management/ui/DepartmentManagementFrame.java
@@ -1,71 +1,25 @@
 package student_management.ui;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
 import student_management.dto.Department;
 import student_management.ui.panel.AbsItemPanel;
 import student_management.ui.panel.DepartmentPanel;
 
 @SuppressWarnings("serial")
-public class DepartmentManagementFrame extends JFrame implements ActionListener {
+public class DepartmentManagementFrame extends AbsManagementFrame<Department> {
+	private DepartmentPanel deptPanel;
 	
-	private JPanel contentPane;
-	private AbsItemPanel<Department> pCenter;
-	private JPanel pSouth;
-	private JButton btnAdd;
-	private JButton btnCancel;
-
 	public DepartmentManagementFrame() {
-		initialize();
-	}
-	
-	private void initialize() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		pCenter = new DepartmentPanel();
-		contentPane.add(pCenter, BorderLayout.CENTER);
-		
-		pSouth = new JPanel();
-		contentPane.add(pSouth, BorderLayout.SOUTH);
-		
-		btnAdd = new JButton("추가");
-		btnAdd.addActionListener(this);
-		pSouth.add(btnAdd);
-		
-		btnCancel = new JButton("취소");
-		btnCancel.addActionListener(this);
-		pSouth.add(btnCancel);
+		deptPanel = new DepartmentPanel();
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnCancel) {
-			btnCancelActionPerformed(e);
-		}
-		if (e.getSource() == btnAdd) {
-			btnAddActionPerformed(e);
-		}
+	@Override
+	protected AbsItemPanel<Department> createItemPanel() {
+		return deptPanel;
 	}
-	protected void btnAddActionPerformed(ActionEvent e) {
-		JOptionPane.showMessageDialog(null, getItem());
-	}
-	protected void btnCancelActionPerformed(ActionEvent e) {
-		pCenter.clearTf();
+
+	@Override
+	protected Department getItem() {
+		return deptPanel.getItem();
 	}
 	
-	private Department getItem() {
-		return pCenter.getItem();
-	}
 }
